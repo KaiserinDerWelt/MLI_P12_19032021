d85b9163ca827e1ad17008d89ee9342cf3ccd598
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-03-23 13:26:45 -0600

    feat: :construction: render left menu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Topmenu from './menu/Topmenu';
+import Sidebarleft from './sidebar/Sidebarleft';
+import Welcome from './welcome/Welcome';
 
-class Dashboard extends React.Component{
-    render(){
-        return(
+class Dashboard extends React.Component {
+    render() {
+        return (
             <>
-       <div className="container-fluid">
-           {/*Calling components here*/}
-           <Topmenu />
-       </div>
+                <div className="container-fluid">
+                    {/*Calling components here*/}
+                    <Topmenu />
+                    <Sidebarleft />
+                </div>
             </>
         )
     }
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -5,6 +5,9 @@ class Welcome extends React.Component{
     render(){
         return(
             <>
+            <div className="container welcomecontainer">
+                <h1>HELLO WORLD</h1>
+            </div>
             </>
         )
     }
