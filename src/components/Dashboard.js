221850eadcc9300f80c0f735f1e9304b42702b74
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-03-23 11:40:37 -0600

    feat: :construction: render Topmenu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-//import Average from '../averagespeed/Average';
+import Topmenu from './menu/Topmenu';
 
 class Dashboard extends React.Component{
     render(){
         return(
             <>
-        
+       <div className="container-fluid">
+           {/*Calling components here*/}
+           <Topmenu />
+       </div>
             </>
         )
     }
--- src/components/menu/Topmenu.js
+++ src/components/menu/Topmenu.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 class Topmenu extends React.Component{
     render(){
         return(
 <>
+<div className="scrollmenu">
+<NavLink to="/" className="main-nav-logo">
+<img className="main-nav-logo-image" src="./img/Logo.png" alt="Sports-Logo"/></NavLink>
+  <a href="#home">Home</a>
+  <a href="#profile">Profile</a>
+  <a href="#settings">Settings</a>
+  <a href="#community">Community</a>
+</div>
 </>
         )
     }
 }
 
 export  default Topmenu;
+
